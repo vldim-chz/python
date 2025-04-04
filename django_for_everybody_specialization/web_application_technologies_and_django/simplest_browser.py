f3e6ef87de2be911003e953379c78590252ea387
vldim-chz <vldim.chz@outlook.com>
2025-04-03 22:32:31 -0400

    Code that shows you how to use socket module in python for retrieving a web page from the internet.

--- django_for_everybody_specialization/web_application_technologies_and_django/simplest_browser.py
+++ django_for_everybody_specialization/web_application_technologies_and_django/simplest_browser.py
@@ -1,12 +1,12 @@
 import socket
 
-from pycparser.c_ast import While
-
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client_socket.connect(('data.pr4e.org', 80))
 
 # alternative to encode(utf-8)
 # cmd = b'GET http://data.pr4e.org/page1.htm HTTP/1.0\r\n\r\n'
+# In case you want to add some headers then you should do something like this
+# 'GET /path/to/resource HTTP/1.1\r\nHost: www.example.com\r\nUser-Agent: PythonSocketClient/1.0\r\nAccept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8\r\nAccept-Language: en-US,en;q=0.5\r\nConnection: close\r\n\r\n'
 cmd = 'GET http://data.pr4e.org/page1.htm HTTP/1.0\r\n\r\n'.encode('utf-8')
 client_socket.send(cmd)
 
